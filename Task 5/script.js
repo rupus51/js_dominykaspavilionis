e4e50379d013e16dbf051af06d3c28ab3847460e
rupus51 <157066284+rupus51@users.noreply.github.com>
2024-01-18 23:14:34 +0200

    Add files via upload

---
+++ Task 5/script.js
@@ -0,0 +1,31 @@
+/* ------------------------------ TASK 5 -----------------------------------
+Turimas "employee" objektas.
+
+Parašykite funkcijas, kurios atliks nurodytas užduotis:
+1. funkcija "getEmployeeFullName" - priims "employee" objektą ir grąžins darbuotojo
+pilną vardą (vardas ir pavardė kartu).
+2. funkcija "updateEmployeeSalary" - priims "employee" objektą ir naują atlyginimą,
+atnaujins atlyginimą objekte ir grąžins atnaujintą objektą.
+-------------------------------------------------------------------------- */
+
+const employee = {
+  firstName: "John",
+  lastName: "Doe",
+  position: "Software Developer",
+  salary: 5000,
+};
+
+function getEmployeeFullName() {
+  return `${employee.firstName} ${employee.lastName}`;
+}
+
+function updateEmployeeSalary(employee, updatedSalary) {
+  if (!employee || updatedSalary === undefined) {
+    return employee;
+  }
+
+  return { ...employee, salary: updatedSalary };
+}
+
+console.log(getEmployeeFullName(employee)); // Rezultatas: 'John Doe'
+console.log(updateEmployeeSalary(employee, 6000)); // Rezultatas: employee object with updated salary
